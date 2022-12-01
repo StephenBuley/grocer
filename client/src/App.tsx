f89463e846fccb7bbf84dad5ded24ca8595eccb4
Stephen Buley <stephen.a.buley@gmail.com>
2022-12-01 20:43:30 +0100

    start mongodb implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 
 function App() {
+
+  type TResponse = {
+    string: string
+  }
+
+  async function handleClick() {
+    const response = await fetch("http://localhost:5002")
+    const data: TResponse  = await response.json()
+    console.log(data)
+  }
+
   return (
     <div className="App">
       <div className="title"
-           onClick={() => console.log("clicked!")}>Hello World</div>
-    </div>  
+           onClick={handleClick}>
+            Hello World
+      </div>
+    </div>
   )
 }
 
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,17 @@
-import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 import express from "express";
 import process from "process";
+import bodyParser from 'body-parser';
+import cors from "cors";
+
+const uri = `mongodb+srv://StephenBuleyDB:${process.env.DB_PASSWORD}@cluster0.4kodetu.mongodb.net/?retryWrites=true&w=majority`;
+
 const PORT = process.env.PORT
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {

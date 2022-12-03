813506cdd0dbf72979ee481faccca1c914b9a65c
Stephen Buley <stephen.a.buley@gmail.com>
2022-12-03 18:39:41 +0100

    flesh out new list modal functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,15 @@ import { NewListModal } from './NewListModal'
 
 function App() {
   const [inModal, setInModal] = useState<boolean>(false)
+  const [listName, setListName] = useState("")
 
   function handleClick() {
     setInModal(true)
   }
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setListName(e.target.value)
+    }
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
@@ -18,7 +23,11 @@ function App() {
     <div className="App">
       <button className="btn__new-list"
               onClick={handleClick}>Create New List</button>
-      {inModal && <NewListModal handleSubmit={handleSubmit}/>}
+      {inModal && <NewListModal 
+                    handleSubmit={handleSubmit}
+                    handleChange={handleChange}
+                    listName={listName}/>}
+      <div className="list">{listName}</div>
     </div>
   )
 }
--- client/src/NewListModal.tsx
+++ client/src/NewListModal.tsx
@@ -1,15 +1,27 @@
-import { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 
 type ModalProps = {
     handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    listName: string
 }
 
-export function NewListModal({handleSubmit}: ModalProps): ReactElement {
+export function NewListModal({
+    handleSubmit, 
+    handleChange, 
+    listName
+}: ModalProps): ReactElement {
 
     return (
         <form className="modal__new-list" onSubmit={(e) => handleSubmit(e)}>
             <label htmlFor="list-name">New List Name</label>
-            <input type="text" name="list-name" id="list-name" />
+            <input 
+                type="text" 
+                name="list-name" 
+                value={listName}
+                id="list-name"
+                onChange={(e) => handleChange(e)}
+            />
             <button type="submit">Submit</button>
         </form>
     )
